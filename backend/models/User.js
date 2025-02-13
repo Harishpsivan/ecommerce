f02e31da1fe2e36f8fbab527d89dbe7ac65b43e4
rakshya64 <rakchya64@gmail.com>
2025-02-13 00:08:44 -0500

    added isadmin option on schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     name:  { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    isAdmin:  { type: Boolean, default:false },
 });
 
 // Use `mongoose.models` to prevent overwriting the model
